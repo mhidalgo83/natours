e404494e8b39d51c12a1f85531d07aabf78a23da
Matt <makeshifthidalgo@hotmail.com>
2019-11-06 11:20:41 -0700

    delete route implemented

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: 'fail',
-      message: 'invalid id'
+      message: 'Invalid ID'
     });
   }
   res.status(200).json({
@@ -83,6 +83,19 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     data: {
       tour: 'updated tour'
     }
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null
   });
 });
 
