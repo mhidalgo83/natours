ba581fe8f22794b17e926cc8843d0c06d3af7239
Matt <makeshifthidalgo@hotmail.com>
2019-11-08 08:25:10 -0700

    created middleware to check name and price of tour

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,10 +5,15 @@ const router = express.Router();
 
 router.param('id', tourController.checkID);
 
+//Create a checkBody middleware function
+//Check if body contains name and price properties
+//If not, send back 400 (bad request)
+//Add it to the post handler stack
+
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.createTour);
+  .post(tourController.checkBody, tourController.createTour);
 
 router
   .route('/:id')
