2638aa39996ff868230a2d69f05f7c69cfdb1cad
Matt <makeshifthidalgo@hotmail.com>
2019-11-08 07:57:28 -0700

    created middleware to validate tour id

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,13 +1,24 @@
-const fs = require("fs");
+const fs = require('fs');
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 );
+
+exports.checkID = (req, res, next, val) => {
+  console.log(`Tour ID is: ${val}`);
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID'
+    });
+  }
+  next();
+};
 
 //Route handlers
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
   res.status(200).json({
-    status: "success",
+    status: 'success',
     requestAt: req.requestTime,
     results: tours.length,
     data: {
@@ -18,15 +29,9 @@ exports.getAllTours = (req, res) => {
 
 exports.getTour = (req, res) => {
   const id = req.params.id * 1;
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "invalid id"
-    });
-  }
   const tour = tours.find(el => el.id === id);
   res.status(200).json({
-    status: "success",
+    status: 'success',
     results: tour,
     data: {
       tour
@@ -44,7 +49,7 @@ exports.createTour = (req, res) => {
     JSON.stringify(tours),
     err => {
       res.status(201).json({
-        status: "success",
+        status: 'success',
         data: {
           tour: newTour
         }
@@ -54,29 +59,17 @@ exports.createTour = (req, res) => {
 };
 
 exports.updateTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID"
-    });
-  }
   res.status(200).json({
-    status: "success",
+    status: 'success',
     data: {
-      tour: "updated tour"
+      tour: 'updated tour'
     }
   });
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID"
-    });
-  }
   res.status(204).json({
-    status: "success",
+    status: 'success',
     data: null
   });
 };
