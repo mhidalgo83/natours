15ed773f761134361ac0e17f721fa61e7d6eb40f
Matt <makeshifthidalgo@hotmail.com>
2019-11-11 13:57:14 -0700

    added .env file for environmental vars, new scripts for dev and prod

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 //Middleware
-app.use(morgan('dev'));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
@@ -24,4 +30,3 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 module.exports = app;
-
