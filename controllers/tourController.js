b6dbff0f8fa77dc1c53f59b09b905062890b5251
Matt <makeshifthidalgo@hotmail.com>
2019-11-19 17:24:10 -0700

    added update controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -55,18 +55,31 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: 'updated tour'
-    }
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    res.status(204).json({
+      status: 'success',
+      data: null
+    });
+  } catch (err) {}
 };
