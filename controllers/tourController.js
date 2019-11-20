d1df037e32e20a935974085bd9910f983b4ef371
Matt <makeshifthidalgo@hotmail.com>
2019-11-20 15:32:44 -0700

    added pagination to api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -18,15 +18,33 @@ exports.getAllTours = async (req, res) => {
 
     let query = Tour.find(JSON.parse(queryStr));
     console.log(req.query);
-    //{difficulty: "easy", duration:{$gte: 5}}
+   
 
     //Sorting...
-    if(req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ")
-      
-      query = query.sort(sortBy)
-    } else{
-      query=query.sort("-createdAt")
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //Field limiting...
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    //Pagination...
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
     }
 
     //Execute query...
