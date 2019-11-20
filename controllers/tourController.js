731a77e5956c54730a5dd826bf8212fe1ce9520c
Matt <makeshifthidalgo@hotmail.com>
2019-11-20 12:34:40 -0700

    built query obj for api

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,7 +3,17 @@ const Tour = require('../models/tourModel');
 //Route handlers
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //Build query...
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+
+    //Execute query...
+    const tours = await query;
+
+    //Send response...
     res.status(200).json({
       status: 'success',
       requestAt: req.requestTime,
