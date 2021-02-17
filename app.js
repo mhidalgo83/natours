62ee9ea424d76f84663d8af242a4b02ae3c83ca0
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2021-02-17 12:00:34 -0700

    first get request complete

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 const express = require('express');
-const morgan = require('morgan');
+const fs = require('fs');
 
 const app = express();
-const tourRouter = require('./routes/tourRoutes');
-const userRouter = require('./routes/userRoutes');
 
-//Middleware
+// app.get('/', (req, res) => {
+//   res.status(200).json({ msg: 'Hello', app: 'Natours' });
+// });
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoint');
+// });
 
-app.use(express.json());
-
-app.use(express.static(`${__dirname}/public`));
-
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+console.log(tours);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
+const port = 3000;
+app.listen(3000, () => {
+  console.log(`App running on port ${port}`);
 });
-
-//Routes
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
-
-module.exports = app;
