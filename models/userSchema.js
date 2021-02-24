5b5f8b98055147dce8037f2d57e2620623a65bda
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2021-02-24 14:06:36 -0700

    bcrypt installed, passwords salted and hashed

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -24,7 +25,25 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your challenge'],
+    validate: {
+      // This only works on CREATE and SAVE!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same.',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run this function was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  // Only used to confirm the password, can be deleted after used, don't persist to db
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
