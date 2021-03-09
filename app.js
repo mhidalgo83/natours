bae60bbdcb0861d379e5c09e76a1d24277c3fe46
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2021-03-09 16:39:31 -0700

    security middlewares implemented

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const errorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -11,20 +15,51 @@ const AppError = require('./utils/error');
 const app = express();
 
 // Middlewares
+
+// Sets security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limits requests to API
 const limiter = rateLimit({
-  max: 3,
+  max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour',
 });
 
 app.use('/api', limiter);
-app.use(express.json());
+
+// Body parser
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middlewares
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
